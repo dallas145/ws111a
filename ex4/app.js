5b157265cf5626130dde9e3b13594239bf4bc8a0
dallas145 <mikelg512@gmail.com>
2022-10-28 14:33:33 +0800

    1028

--- ex4/app.js
+++ ex4/app.js
@@ -1,19 +1,37 @@
 import { Application, Router, send } from "https://deno.land/x/oak/mod.ts";
+import { DB } from "https://deno.land/x/sqlite/mod.ts";
 
+const db = new DB("Mydb.db")
 const app = new Application()
-
 const router = new Router()
 
-router.post('/index', main)
+router.get('/', mainpage)
+.get('/sqlcmd/:cmd', sqlcmd)
+.get('/public/(.*)', pub)
 
 app.use(router.routes())
-app.use(router.allowMethods())
+app.use(router.allowedMethods())
 
-app.use(async (ctx, next) => {
-    await next()
-    console.log('path=', ctx.request.url.pathname)
+async function mainpage(ctx){
+    ctx.response.redirect('/public/')
+}
+
+async function pub(ctx){
+    console.log(ctx.request.url.pathname)
     await send(ctx, ctx.request.url.pathname,{
-        root: `${Deno.cwd()}/public/`,
-        index: "index.html",
+        root:`${Deno.cwd()}/`,
+        index:"index.html",
     })
-})
+}
+
+async function sqlcmd(ctx){
+    let command = ctx.params['cmd']
+    console.log(command)
+    let result = db.query(command)
+    console.log(result)
+    ctx.response.type='application/json'
+    ctx.response.body = result
+}
+
+console.log('Server run at http://127.0.0.1:8000')
+await app.listen({ port: 8000 })
--- ex4/public/SsqliteManager.js
+++ ex4/public/SsqliteManager.js
@@ -1,18 +1,8 @@
-import { DB } from "https://deno.land/x/sqlite/mod.ts";
-
-try {
-    Deno.remove("mydb.db")
-}catch(e){
-    console.log("file not exit!")
-}
-const db = new DB("mydb.db")
-while (true) {
-    let command = prompt('sql?')
-    if (command=='exit') break
-    try {
-        let r = db.query(command)
-        console.log(r)
-    } catch (error) {
-        console.log('error=', error)
-    }
+async function sqlRun() {
+    let command = document.getElementById('command').value
+    let final = document.getElementById('final')
+    console.log(command)
+    let r = await window.fetch(`/sqlcmd/${command}`)
+    let obj = await r.json()
+    final.innerText = JSON.stringify(obj, null, 2)
 }
